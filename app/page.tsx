32e098b8407ec83d02b52e22952e5ba287f8b4dd
George Bobby <mailto.georgebobby@gmail.com>
2024-11-18 07:32:03 +0530

    tail

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import ProductCard from './components/ProductCard';
 export default function Home() {
   return (
     <main>
-      <h1>Hello World</h1>
+      {/* <h1>Hello World</h1> */}
       <Link href="/users">Users</Link>
       <ProductCard/>
     </main>
