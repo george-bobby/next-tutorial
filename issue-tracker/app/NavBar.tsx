d0abd400aea96b916eca2fe6107c49c2fc7e97a5
George Bobby <mailto.georgebobby@gmail.com>
2024-11-20 17:40:06 +0530

    initial

---
+++ issue-tracker/app/NavBar.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import React from 'react'
+
+const NavBar = () => {
+  return (
+      <nav>
+          <Link href="/">Logo</Link>
+    </nav>
+  )
+}
+
+export default NavBar
